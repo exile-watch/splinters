dcf8505d36dbab172352e80f3fe268373d622286
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-06 17:50:17 +0100

    feat(packages): update release.config.js in semantic-release-config

--- packages/semantic-release-config/release.config.js
+++ packages/semantic-release-config/release.config.js
@@ -7,5 +7,6 @@ module.exports = {
     mergePattern: /^Merge pull request #(\d+) from (.*)$/,
     mergeCorrespondence: ["id", "source"]
   },
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
   plugins
 };
