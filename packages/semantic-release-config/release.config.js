e12d05fa949aa0d82e3788c9175a929d9cceaefa
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-06 19:27:14 +0100

    refactor(packages): check .releaserc

--- packages/semantic-release-config/release.config.js
+++
@@ -1,9 +0,0 @@
-const plugins = require("./plugins");
-
-/**
- * @type {import('semantic-release').GlobalConfig}
- */
-module.exports = {
-  branches: ['main', {name: 'beta', prerelease: true}],
-  plugins
-};
