48096479b9789da11a7e3cc051dcad4b692d1516
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-11 09:12:01 +0100

    refactor(rollup-config): remove dist

--- packages/rollup-config/dist/externalMatcher.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Gets the base package name, including scope (if applicable) from an import path
- * @param id full import path used in an import processed by Rollup
- * @returns package name, or null if the import path is not a node_module
- * @see https://rollupjs.org/guide/en/#external
- */
-export declare const getBasePackage: (id: string) => string | null;
