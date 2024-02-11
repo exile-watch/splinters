c43c778c8793fff8bc0b2303d7116e951cfccd73
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-11 22:13:57 +0100

    chore(setup): add bin

--- packages/storybook-config/src/index.mjs
+++ packages/storybook-config/src/index.mjs
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
-import {execSync} from 'child_process'
+const {execSync} = require('child_process')
+const path = require('path')
 
 const port = 6006
 const outputDir = 'dist-storybook'
 
 const execProps = {encoding: 'utf-8', stdio: 'inherit'}
 const execAction = String(process.argv[2]) //gets 'start' or 'build' command from calling package
+const sbConfigDir = path.join(__dirname, '.storybook')
 
 if (execAction === 'start') {
-  execSync(`npx start-storybook --port=${port}`, execProps)
+  execSync(`npx storybook dev --port=${port} --config-dir=${sbConfigDir}`, execProps)
 } else if (execAction === 'build') {
   execSync(`npx build-storybook --output-dir=${outputDir}`, execProps)
 }
