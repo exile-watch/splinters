21f70c2fa6bd59ef184b743fa8b20740d10dac06
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-11 11:19:22 +0100

    refactor(pnpm): experiment with preserveSymlinks

--- packages/rollup-config/src/rollup.config.ts
+++ packages/rollup-config/src/rollup.config.ts
@@ -136,6 +136,8 @@ const createRollupConfig = async (options: CreateRollupConfigOptions = {}): Prom
       },
     ],
 
+    preserveSymlinks: true,
+
     /********************************************
      * Configure Plugins
      ********************************************/
