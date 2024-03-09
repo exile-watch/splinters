1cb5023fc6b6ad4da1058467bfe1eb2116fc37c4
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-03-09 21:04:46 +0100

    chore(deps): bump @exile-watch/writ-react to latest

--- packages/unit-testing-config/src/test-setup.tsx
+++ packages/unit-testing-config/src/test-setup.tsx
@@ -1,11 +1,11 @@
 import { render as rtlRender } from '@testing-library/react';
-import { WritProvider } from '@exile-watch/writ-react';
+import { MockWritProvider } from '@exile-watch/writ-react';
 import React, {ReactNode} from 'react';
 
 // Override the default render method
 const render = (ui: ReactNode, options = {}) =>
   rtlRender(ui, {
-    wrapper: ({ children }) => <WritProvider>{children}</WritProvider>,
+    wrapper: ({ children }) => <MockWritProvider>{children}</MockWritProvider>,
     ...options,
   });
 
