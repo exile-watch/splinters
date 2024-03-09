5f907718d3311dae64b47018fd1b8aca27ef2284
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-03-09 08:42:19 +0100

    refactor: wrap config with defineConfig

--- packages/unit-testing-config/src/vitest.config.ts
+++ packages/unit-testing-config/src/vitest.config.ts
@@ -1,13 +1,13 @@
-import { mergeConfig } from 'vitest/config'
+import {defineWorkspace, mergeConfig, defineConfig} from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-const defaultConfig =  {
+const defaultConfig = defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./test-setup.tsx']
   }
-}
+})
 
 export {defaultConfig, mergeConfig}
