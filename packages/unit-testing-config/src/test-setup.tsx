8d0894bba07b4cbf428281adfc2c9541c2aaa65c
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-03-10 07:37:09 +0100

    refactor: experiment with silencing mantine error

--- packages/unit-testing-config/src/test-setup.tsx
+++ packages/unit-testing-config/src/test-setup.tsx
@@ -2,6 +2,15 @@ import { render as rtlRender } from '@testing-library/react';
 import { MockWritProvider } from '@exile-watch/writ-react';
 import React, {ReactNode} from 'react';
 
+process.on('uncaughtException', (error) => {
+  if (error.message.includes("@mantine/core: MantineProvider was not found in component tree, make sure you have it in your app")) {
+    return;
+  }
+
+  // For all other uncaught exceptions, you might want to log them or handle them differently
+  return error
+});
+
 // Override the default render method
 const render = (ui: ReactNode, options = {}) =>
   rtlRender(ui, {
