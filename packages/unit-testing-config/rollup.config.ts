a69a061ede19d7676d20655a0c157a4fcb884a9f
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-03-10 07:07:52 +0100

    refactor: remove externals

--- packages/unit-testing-config/rollup.config.ts
+++ packages/unit-testing-config/rollup.config.ts
@@ -1,6 +1,5 @@
 import createRollupConfig from '@exile-watch/rollup-config'
 
 export default createRollupConfig({
-  outputFormats: ['module'],
-  externals: ["@exile-watch/writ-react"]
+  outputFormats: ['module']
 })
