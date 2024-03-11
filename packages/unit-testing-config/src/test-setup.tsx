5598e889c7ebba6e974884b93ddfab6fe70324fd
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-03-11 22:27:29 +0100

    chore(deps): experiment with mocking next font

--- packages/unit-testing-config/src/test-setup.tsx
+++ packages/unit-testing-config/src/test-setup.tsx
@@ -1,6 +1,20 @@
 import { render as rtlRender } from '@testing-library/react';
 import { MockWritProvider } from '@exile-watch/writ-react';
 import React, {ReactNode} from 'react';
+import { vi } from 'vitest';
+
+const mockLocalFont = vi.fn().mockImplementation(() => {
+  return {
+    src: 'mocked-font-path',
+    variable: '--global-font-fontin',
+  };
+});
+
+vi.mock('next/font/local', () => {
+  return {
+    default: mockLocalFont
+  };
+});
 
 // Override the default render method
 const render = (ui: ReactNode, options = {}) =>
