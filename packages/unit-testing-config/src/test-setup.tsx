0789b8ba04b71835cbd9fa8400ba206147c3e677
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-03-12 16:25:45 +0100

    feat: extend config

--- packages/unit-testing-config/src/test-setup.tsx
+++ packages/unit-testing-config/src/test-setup.tsx
@@ -1,20 +1,6 @@
 import { render as rtlRender } from '@testing-library/react';
 import { MockWritProvider } from '@exile-watch/writ-react';
 import React, {ReactNode} from 'react';
-import { vi } from 'vitest';
-
-const mockLocalFont = vi.fn().mockImplementation(() => {
-  return {
-    src: 'mocked-font-path',
-    variable: '--global-font-fontin',
-  };
-});
-
-vi.mock('next/font/local', () => {
-  return {
-    default: mockLocalFont
-  };
-});
 
 // Override the default render method
 const render = (ui: ReactNode, options = {}) =>
--- packages/unit-testing-config/src/vitest.config.ts
+++ packages/unit-testing-config/src/vitest.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vitest/config'
+import { mergeConfig, defineConfig, UserConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import {PluginOption} from "vite";
 
-const defaultConfig = defineConfig({
-  plugins: [tsconfigPaths()],
+const defaultConfig = (config?: UserConfig) => defineConfig({
+  ...config,
+  plugins: [
+    ...config?.plugins as PluginOption[],
+    tsconfigPaths()
+  ],
   test: {
+    ...config?.test,
     globals: true,
     environment: 'jsdom',
     setupFiles: [
+      ...config?.test?.setupFiles as string | string[],
       '@testing-library/jest-dom',
       '@exile-watch/unit-testing-config/dist/test-setup.mjs'
     ]
   }
 })
 
-export {defaultConfig}
+export {mergeConfig, defaultConfig}
