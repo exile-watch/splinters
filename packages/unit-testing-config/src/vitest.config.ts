9a1eea5c499721b04e501f5c6283463193e69d37
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-03-12 17:22:30 +0100

    fix: remove mergeConfig export

--- packages/unit-testing-config/src/vitest.config.ts
+++ packages/unit-testing-config/src/vitest.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig, UserConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import {PluginOption} from "vite";
 
-const defaultConfig = (config?: UserConfig) => defineConfig({
-  ...config,
+const defaultConfig = defineConfig({
   plugins: [
-    ...config?.plugins as PluginOption[],
     tsconfigPaths()
   ],
   test: {
-    ...config?.test,
     globals: true,
     environment: 'jsdom',
     setupFiles: [
-      ...config?.test?.setupFiles as string | string[],
       '@testing-library/jest-dom',
       '@exile-watch/unit-testing-config/dist/test-setup.mjs'
     ]
