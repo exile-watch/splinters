46c3a7a71a656646a9b1e9a5ad5806a15643811b
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-10-06 12:58:46 +0200

    fix: handle ES Module import for conventional-changelog-conventionalcommits

--- packages/conventional-changelog-config/index.js
+++ packages/conventional-changelog-config/index.js
@@ -1,49 +1,62 @@
-const config = require('conventional-changelog-conventionalcommits')
+let configModule;
 
-module.exports = config({
-  commitUrlFormat: '{{host}}/{{owner}}/{{repository}}/commit/{{hash}}',
-  compareUrlFormat: '{{host}}/{{owner}}/{{repository}}/compare/{{previousTag}}...{{currentTag}}',
+try {
+  configModule = require("conventional-changelog-conventionalcommits");
+} catch (err) {
+  // If the module is an ES Module, use dynamic import() as a fallback.
+  if (err.code === "ERR_REQUIRE_ESM") {
+    configModule = async () =>
+      (await import("conventional-changelog-conventionalcommits")).default;
+  } else {
+    throw err;
+  }
+}
+
+module.exports = configModule({
+  commitUrlFormat: "{{host}}/{{owner}}/{{repository}}/commit/{{hash}}",
+  compareUrlFormat:
+    "{{host}}/{{owner}}/{{repository}}/compare/{{previousTag}}...{{currentTag}}",
   types: [
     {
-      section: '✨ Features',
-      type: 'feat',
+      section: "✨ Features",
+      type: "feat",
     },
     {
-      section: '🐞 Bug Fixes',
-      type: 'fix',
+      section: "🐞 Bug Fixes",
+      type: "fix",
     },
     {
-      section: '📄 Documentation',
-      type: 'docs',
+      section: "📄 Documentation",
+      type: "docs",
     },
     {
-      section: '⚙️ Internal',
-      type: 'chore',
+      section: "⚙️ Internal",
+      type: "chore",
     },
     {
-      section: '⚙️ Code Quality',
-      type: 'style',
+      section: "⚙️ Code Quality",
+      type: "style",
     },
     {
-      section: '⚙️ Code Quality',
-      type: 'refactor',
+      section: "⚙️ Code Quality",
+      type: "refactor",
     },
     {
-      section: '⚙️ Code Quality',
-      type: 'perf',
+      section: "⚙️ Code Quality",
+      type: "perf",
     },
     {
-      section: '🤖 CI/CD',
-      type: 'test',
+      section: "🤖 CI/CD",
+      type: "test",
     },
     {
-      section: '🤖 CI/CD',
-      type: 'ci',
+      section: "🤖 CI/CD",
+      type: "ci",
     },
     {
-      section: '🤖 CI/CD',
-      type: 'build',
+      section: "🤖 CI/CD",
+      type: "build",
     },
   ],
-  userUrlFormat: '{{host}}/{{user}}',
-})
+  userUrlFormat: "{{host}}/{{user}}",
+});
